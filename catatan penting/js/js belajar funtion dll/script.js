f68a5feb9768766fab97ce28a3e6d85ae803b254
XuraTempest <farenzawiyantnaufandra@gmail.com>
2026-07-04 13:40:00 +0700

    Add files via upload

---
+++ catatan penting/js/js belajar funtion dll/script.js
@@ -0,0 +1,18 @@
+function contoh(kwargs) {
+    for (let key in kwargs) {
+        console.log(key, kwargs[key]);
+    }
+}
+
+contoh({ nama bambang, umur 20 });
+
+let bambang = kimak
+
+function bocah() {
+
+    let bambang = puacak
+    return bambang
+}
+
+console.log(bocah())
+console.log(W)
